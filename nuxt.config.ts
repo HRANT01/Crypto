82b8d2c8503f51cc554ea6caf405dc53c98a2a15
hrant <hrantalexanyan13@gmail.com>
2024-06-28 18:10:46 +0400

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
   app: {
     baseURL: '/Crypto/',
     buildAssetsDir: 'assets'
